7c6016d40f1c584a216256f5d62e2985b7958834
RajaVamshi <vamshi199189@gmail.com>
2024-05-11 17:59:13 +0530

    react project

---
+++ react_fronted/src/components/FooterComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import  '../App.css';
+
+function FooterComponent() {
+  return (
+    <div>
+                <footer className='footer'>
+            <span>All Right Reserved 2024 &copy; RAJ_VAMSHI</span>
+          </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
---
+++ react_fronted/src/components/HeaderComponent.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function HeaderComponent() {
+  return (
+    <div>
+             <header >
+
+<div className='navbar navbar-expand-lg navbar-dark bg-dark'>
+    <div className='container'>
+    <a href='' className='navbar-brand'> Employee Management System</a>
+    </div>                            
+</div>
+
+</header>
+
+    </div>
+  )
+}
+
+export default HeaderComponent
